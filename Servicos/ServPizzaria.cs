223139317da7284950506707e0aa9f651818c672
eduardo <eduardocmeneghel@gmail.com>
2024-03-27 21:50:31 -0300

    Criando Crud da pizzaria

---
+++ Servicos/ServPizzaria.cs
@@ -0,0 +1,31 @@
+﻿using Entidades;
+using Repositorio;
+
+namespace Servicos
+{
+    public interface IServPizzaria
+    {
+
+    }
+
+    public class ServPizzaria
+    {
+        private IRepoPizzaria _repoPizzaria;
+
+        public ServPizzaria(IRepoPizzaria repoPizzaria)
+        {
+            _repoPizzaria = repoPizzaria;
+        }
+
+        public void Inserir(InserirPizzariaDTO inserirPizzariaDto)
+        {
+            var pizzaria = new Pizzaria();
+
+            pizzaria.Nome = inserirPizzariaDto.Nome;
+            pizzaria.Telefone = inserirPizzariaDto.Telefone;
+            pizzaria.Endereco = inserirPizzariaDto.Endereco;
+
+            _repoPizzaria.Inserir(pizzaria);
+        }
+    }
+}
